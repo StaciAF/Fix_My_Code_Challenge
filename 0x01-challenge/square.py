b2f256219835cedea82667025ab2a8571e003ee7
StaciAF <aaen.it19@gmail.com>
2020-09-28 05:51:26 +0000

    updated string rep

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -23,7 +23,7 @@ class Square:
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_Square())
     print(s.perimeter_of_my_Square())
