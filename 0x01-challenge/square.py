60ddc1dd514154839b6a58bb108dae1dfb5f07b5
StaciAF <aaen.it19@gmail.com>
2020-10-01 16:12:57 +0000

    back to original function names

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ nothing imported """
 
 
-class Square:
+class square():
     """ defines Square class """
     width = 0
     height = 0
@@ -26,7 +26,7 @@ class Square:
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
-    print(s.area_of_my_Square())
-    print(s.perimeter_of_my_Square())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
