3b25394eb7fbd5bd6bc0b1dd6ee8f93871e94ca5
StaciAF <aaen.it19@gmail.com>
2020-10-01 16:20:43 +0000

    fixing naming conventions

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ nothing imported """
 
 
-class square:
+class Square:
     """ defines Square class """
     width = 0
     height = 0
@@ -12,11 +12,11 @@ class square:
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_Square(self):
+    def area_of_my_square(self):
         """ Area of the square """
         return (self.width * self.height)
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
